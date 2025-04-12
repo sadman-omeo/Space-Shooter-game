298132526a2204e9c2ac1b909a98f064871b7dc9
SADMAN AHMED OMEO <sadmanahmed167@gmail.com>
2025-04-12 22:06:37 +0600

    Update space shooter base.py

--- space shooter base.py
+++ space shooter base.py
@@ -30,3 +30,48 @@ game_over = False
 #Handling play pause end button using mouse
 W_Width, W_Height = 500, 500
 
+
+
+class FallingCircle:
+    def __init__(self, x, y, special = False, color = (1, 0, 0)):
+        self.x = x
+        self.y = y
+        self.rad = circle_rad
+        self.special = special
+        
+    def move(self):
+        self.y -= circle_speed
+    
+    def update_rad(self):
+        if self.special == True:
+            self.rad = circle_rad + (circle_rad / 4) * (1 + math.sin(time.time() * 2))
+            
+
+
+def spawn_falling_circle():
+    x = random.randint(15, 485)
+    special = random.random() < 0.15
+    new_circle = FallingCircle(x, 500, special)
+    min_dist = circle_rad * 2
+    
+    for c in falling_circles:
+        dist = ((new_circle.x - c.x) ** 2 + (new_circle.y - c.y) ** 2) ** 0.5
+        if dist < min_dist + 10:
+            return
+    falling_circles.append(new_circle)
+
+
+def move_falling_circle():
+    for c in falling_circles:
+        c.move()
+        if c.y < 0:
+            falling_circles.remove(c)
+  
+def draw_falling_circle():
+    for c in falling_circles:
+        if c.special == True:
+            draw_mpc_circle(c.x, c.y, c.rad, (0, 1, 0))
+        else:
+            draw_mpc_circle(c.x, c.y, c.rad, (1, 0, 0))
+        
+        
